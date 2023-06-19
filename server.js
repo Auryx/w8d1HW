26df21b2fa6a80eebb686dd10d7e3088136655ce
Auryx <lilbizb1@gmail.com>
2023-06-18 23:51:47 -0700

    listed errors at the bottom

--- server.js
+++ server.js
@@ -27,3 +27,9 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log(`Listening on Port 3000`)
 })
+
+// list of errors
+// 1. Refused to apply style from '*insert file structure here*' because its MIME type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
+// 2. Error: No default engine was specified and no extension was provided
+// 3. Error: Failed to lookup view "you hit the server js route" in views directory '*insert file structure here*'
+// 4. Error: Failed to lookup view "you hit the pokedex router" in views directory '*insert file structure here*'
